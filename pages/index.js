cc3a5c1e484567e1e3317929ae994a37385cbd9e
Daniel Alves <64676391+DanielFrontEndDev@users.noreply.github.com>
2023-01-28 10:26:59 -0300

    feat: increase title length tag

--- pages/index.js
+++ pages/index.js
@@ -32,7 +32,7 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Omnesbet</title>
+        <title>Omnesbet | Arbitrage Software</title>
         <meta name="description" content="Best Software for Sports Betting Arbitrage" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href={logo.src} />
