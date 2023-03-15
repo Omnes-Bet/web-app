7948d99bdd58e7970644820587188be0b469d586
Daniel Alves <64676391+DanielFrontEndDev@users.noreply.github.com>
2023-03-15 02:19:00 -0300

    feat: handle mask in signup form

--- components/SignUpCard/SignUpCard.js
+++ components/SignUpCard/SignUpCard.js
@@ -8,40 +8,76 @@ import {
   Button,
   Stack,
   CircularProgress,
-  Modal
+  Modal,
 } from "@mui/material";
-import { AdapterDateFns } from '@mui/x-date-pickers/AdapterDateFns';
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
 import { MobileDatePicker } from "@mui/x-date-pickers";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AuthContext } from "../../contexts/authContext";
 
-
 const SignUpCard = () => {
-  const { signup, isLoading } = useContext(AuthContext)
+  const { signup, isLoading } = useContext(AuthContext);
 
-  const [name, setName] = useState('');
-  const [phoneNumber, setPhoneNumber] = useState('');
-  const [email, setEmail] = useState('');
-  const [birthday, setBirthday] = useState('');
-  const [password, setPassword] = useState('');
-  const [promoCode, setPromoCode] = useState('');
+  const [name, setName] = useState("");
+  const [emailError, setEmailError] = useState(false);
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+  const [birthdayError, setBirthdayError] = useState(false);
+  const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState(false);
+  const [promoCode, setPromoCode] = useState("");
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
 
-  const handleChange = (newValue) => {
+  const validateEmail = (email) => {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(String(email).toLowerCase());
+  };
+
+  const handleEmailChange = (event) => {
+    const newEmail = event.target.value;
+    setEmail(newEmail);
+    setEmailError(!validateEmail(newEmail));
+  };
+
+  const validatePassword = (password) => {
+    return password.length >= 8;
+  };
+
+  const handlePasswordChange = (event) => {
+    const newPassword = event.target.value;
+    setPassword(newPassword);
+    setPasswordError(!validatePassword(newPassword));
+  };
+
+  const validateBirthday = (birthday) => {
+    const userBday = birthday?.getFullYear();
+    const today = new Date().getFullYear();
+    const age = today - userBday;
+    return age >= 18;
+  };
+
+  const handleChange = async (newValue) => {
     setBirthday(newValue);
+    const isValidBirthday = validateBirthday(newValue);
+    if (!isValidBirthday) {
+      setBirthdayError(true);
+      return;
+    } else {
+      setBirthdayError(false);
+    }
   };
 
   const handleSubmit = async () => {
-
     const credentials = {
       name: name,
       phoneNumber: phoneNumber,
       email: email,
       birthday: birthday,
       password: password,
-      promoCode: promoCode
+      promoCode: promoCode,
     };
 
     await signup(credentials);
@@ -50,7 +86,6 @@ const SignUpCard = () => {
   useEffect(() => {
     isLoading ? handleOpen() : handleClose();
   }, [isLoading]);
-
 
   return (
     <Card sx={{ maxWidth: 400, margin: "0 auto" }}>
@@ -84,32 +119,47 @@ const SignUpCard = () => {
               noValidate
               autoComplete="off"
             >
-              <TextField 
-                required id="outlined-required" 
+              <TextField
+                required
+                id="outlined-required"
                 label="Full Name"
                 value={name}
                 onChange={(e) => setName(e.target.value)}
               />
               <TextField
-                inputProps={{ inputMode: "numeric", pattern: "[0-9]*" }}
+                required
                 label="Phone Number"
                 value={phoneNumber}
                 onChange={(e) => setPhoneNumber(e.target.value)}
+                type="number"
               />
-              <TextField 
-                required id="outlined-required" 
-                label="email"
+              <TextField
+                required
+                label="Email"
+                type="email"
                 value={email}
-                onChange={(e) => setEmail(e.target.value)}
+                onChange={handleEmailChange}
+                error={emailError}
+                helperText={emailError ? "Please enter a valid email" : ""}
               />
               <MobileDatePicker
+                required
                 label="Date of Birth"
                 inputFormat="MM/dd/yyyy"
                 value={birthday}
                 onChange={handleChange}
-                renderInput={(params) => <TextField {...params} />}
+                renderInput={(params) => (
+                  <TextField
+                    {...params}
+                    error={birthdayError}
+                    helperText={
+                      birthdayError ? "You must be 18 or older to sign up" : ""
+                    }
+                  />
+                )}
               />
               <TextField
+                required
                 width={70}
                 m="auto"
                 id="outlined-password-input"
@@ -117,23 +167,31 @@ const SignUpCard = () => {
                 type="password"
                 autoComplete="current-password"
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handlePasswordChange}
+                error={passwordError}
+                helperText={
+                  passwordError
+                    ? "Password must be at least 8 characters long"
+                    : ""
+                }
               />
-              <TextField 
-                id="outlined-required" 
+              <TextField
+                id="outlined-required"
                 label="Bonus Code"
                 value={promoCode}
                 onChange={(e) => setPromoCode(e.target.value)}
               />
-              <Button 
+              <Button
                 variant="contained"
-                onClick={() => {handleSubmit()}}
-                >
+                onClick={() => {
+                  handleSubmit();
+                }}
+              >
                 Press to Sign Up
               </Button>
             </Box>
           </Stack>
-          </LocalizationProvider>
+        </LocalizationProvider>
       </CardContent>
     </Card>
   );
